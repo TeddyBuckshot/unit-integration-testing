1ab59bb03ceab6564f0df8aadb2dac6cbb2290e2
Sam <samspencer2202@outlook.com>
2021-12-20 23:51:19 +0000

    unit+integration_testing

---
+++ cypress/integration/calculator.spec.js
@@ -0,0 +1,88 @@
+describe("Calculator", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000");
+  })
+
+  it('should have working number buttons', () => {
+    cy.get('#number2').click();
+    cy.get('.display').should('contain', '2');
+  })
+
+  it('number buttons should update the display of the running total', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number3').click();
+    cy.get('.display').should('contain', '3');
+
+  });
+
+  it('arithmetical operations should update the display with the result of the operation', () => {
+    cy.get('#number2').click();
+    cy.get('#operator_add').click();
+    cy.get('#number3').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '5');
+  });
+
+  it('multiple operations be chained together', () => {
+    cy.get('#number4').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number4').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number6').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number1').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '100');
+
+  });
+
+  it('output should be as expected for a positive number', () => {
+    cy.get('#number4').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number4').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '16');
+  });
+  it('output should be as expected for a negative number', () => {
+    cy.get('#number4').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number8').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '-4');
+  });
+  it('output should be as expected for a decimal number', () => {
+    cy.get('#number1').click();
+    cy.get('#decimal').click();
+    cy.get('#number5').click();
+    cy.get('#operator_add').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '3.5');
+  });
+  it('output should be as expected for a large number', () => {
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#number9').click();
+    cy.get('#decimal').click();
+    cy.get('#number9').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number3').click();
+    cy.get('#number2').click();
+    cy.get('#number1').click();
+    cy.get('#decimal').click();
+    cy.get('#number3').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '3212967.87');
+  });
+
+  it('should display zero when multiplied by zero', () => {
+    cy.get('#number9').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number0').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', 'Error');
+  });
+})
